2aaa135d7677f95fae1c9c6af983f1205fefea30
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2024-04-07 17:21:23 +0500

    change api endpoint

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://us-central1-truckers-978b8.cloudfunctions.net/app",
   timeout: 5000,
   withCredentials : true
 });
