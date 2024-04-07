e02cda5e566f92198f50e6b2e4f80869a81c111e
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2024-04-07 23:28:05 +0500

    replace api endpoint

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //baseURL: "https://us-central1-truckers-978b8.cloudfunctions.net/app",
-  baseURL: "http://localhost:8000",
+  baseURL: "https://us-central1-truckers-978b8.cloudfunctions.net/app",
+  //baseURL: "http://localhost:8000",
   timeout: 5000,
   withCredentials : true
 });
--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:8000';
+const URL = 'https://us-central1-truckers-978b8.cloudfunctions.net/app';
 
 export const socket = io(URL, {
     autoConnect : false
